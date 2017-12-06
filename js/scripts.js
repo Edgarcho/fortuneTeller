91d4284fb63a77867d726526a5a7db1089c463b3
ed and os <ed@>
2017-12-05 16:26:32 -0800

    add function and if statement in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
+  $("form#fortune_survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+    var unlucky = 0;
+    var items = 0;
+    var acting = 0;
+    var lucky = 0;
+
+    $("input:checkbox[name=unlucky-event]:checked").each(function(){
+      unlucky += 1
     });
-    $('#transportation_survey').hide();
+      $("input:checkbox[name=unlucky-item]:checked").each(function(){
+      items += 1
+    });
+      $("input:checkbox[name=acting]:checked").each(function(){
+      acting += 1
+    });
+      $("input:checkbox[name=lucky]:checked").each(function(){
+      lucky += 1
+    });
+    $("#responses").show();
+
+    if(unlucky >= 3 && items >= 2){
+      $("#responses").html("<img src=img/cookie.jpg>");
+    }
+    else if (acting >= 2 && lucky >=1){
+      $("#responses").html("<img src=img/cookie3.jpg>");
+    } else {
+      $("#responses").html("<img src=img/cookie2.jpg>").fadeOut(6000);
+
+    }
   });
 });
